29320cd22495ebbfbc0540f6a030cdafc02db0b4
Ivan Ruiz <ivanrur95@hotmail.com>
2021-03-12 16:10:28 -0600

    final de sicsas chatbox con opciones

--- js/chat.js
+++ js/chat.js
@@ -11,18 +11,29 @@ let contenido = null
 let botonRegresar =  document.querySelectorAll('.fa-arrow-left' )
 
 globoChat.addEventListener('click', function () {
+
+    
     mostrarChat: {
         chatBox.className = chatBox.className + (' active')
+        chatBox.style.opacity = 0
+
+        setTimeout(function () {
+            
+            chatBox.style.opacity = 1
+        },100)
     }
         globoStatus = 1
         globoChat.classList = ('d-none')
+        globoChat.style.transition= '.3s ease-in'
+
     for (let i = 0; i < pregunta.length; i++) {
         pregunta[i].addEventListener('click', evento);
+        
     }
   
     for (let i = 0; i < botonRegresar.length; i++) {
         botonRegresar[i].addEventListener('click', regresar)
-    
+        
     }
 })
 
@@ -33,32 +44,50 @@ evento = function (e) {
 
     }
 
-    this.parentElement.className = 'active';
+    this.parentElement.classList = ('preguntas text-center mt-5')
     contenido = this.getAttribute('href');
     document.querySelector(contenido).className = 'd-block'
     contenedorPreguntas.classList = ('d-none')
+    document.querySelector(contenido).style.opacity = '0';
+    document.querySelector(contenido).style.transition = '.2s ease-in';
+    
+
+    setTimeout(function(){
+        document.querySelector(contenido).style.opacity = '1';
+        document.querySelector(contenido).style.transition = '.2s ease-in';
+
+    }, 100)
 
 }
 regresar = function() {
     contenedorPreguntas.classList = ('preguntas text-center')
     document.querySelector(contenido).className = 'd-none'
+    contenedorPreguntas.style.opacity = '0';
+    contenedorPreguntas.style.transition = '.2s ease-in';
+
+    setTimeout(function(){
+        contenedorPreguntas.style.opacity = '1';
+        contenedorPreguntas.style.transition = '.5s ease-in';
+
+    }, 100)
 }
 
 botonMin.addEventListener('click', () => {
     chatBox.classList = ('chat-box min')
+    chatBox.style.transition = '.3s ease-in';
 })
 
 botonMax.addEventListener('click', () =>{
-    botonMax.style.cursor = 'pointer';
     chatBox.classList = ('chat-box active')
 })
 
 botonCerrar.addEventListener('click', () =>{
-    botonCerrar.style.cursor = 'pointer';
     chatBox.classList = 'chat-box'
-    globoChat.classList = ('chat')
-    globoStatus = 0
-    console.log(botonCerrar)
+    chatBox.style.transition= '.3s ease-in'
     
-})
+    globoChat.classList = ('chat')
+    globoChat.style.opacity = 0
+    globoStatus = 0 
 
+
+})
